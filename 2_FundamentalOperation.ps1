126ccabe2c014fb4f0a6a40954b07b637124ccca
ZTI <zhaotianff@163.com>
2018-10-16 12:17:51 +0800

    Fundamental Operation

---
+++ 2_FundamentalOperation.ps1
@@ -0,0 +1,18 @@
+#
+# FundamentalOperation.ps1
+#
+
+#Basic calculation
+1+2
+2*2
+(1+2)*3
+
+#Execute 
+cmd
+notepad
+"E:\Notepad++\notepad++.exe"
+
+#Pass argc
+notepad "F:\project\PowerShellManual.git\trunk\README.md"
+
+
