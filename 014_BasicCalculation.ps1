fd1dfa3ed3fec3f86c72558f1566c1652c7d7a70
ZTI <zhaotianff@163.com>
2018-12-25 12:24:36 +0800

    Arithmetic operators

--- 014_BasicCalculation.ps1
+++ 014_BasicCalculation.ps1
@@ -2,3 +2,33 @@
 # _014_BasicCalculation.ps1
 #
 
+#Arithmetic operators
+#+ - * / % += -= /= %= ++ --
+$sum = 1+1
+$sum
+#Output
+#2
+
+$sum = 3 * 3
+$sum
+#Output
+#9
+
+$sum++
+$sum
+#Output
+#10
+
+$f = 3/2
+$f
+#Output
+#1.5
+
+#Convert to integer
+$i = [int] (3/2)
+$i
+#Output
+#2
+
+#Complex Arithmetic
+#Use .Net System.Math class
